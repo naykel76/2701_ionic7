f1b5099615c53176c55a4b639806c745560546be
Nathan <nathan@naykel.com.au>
2023-04-17 14:24:34 +1000

    lab 3 complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'account',
+    loadComponent: () => import('./account/account.page').then( m => m.AccountPage)
+  },
 ];
