0c6beffd53659d073cfa94dd1bb4f9c49d70ae28
Nathan <nathan@naykel.com.au>
2023-04-23 06:58:48 +1000

    lab 7 task3

---
+++ nk_commands.md
@@ -0,0 +1,3 @@
+tsc item.ts book.ts main.ts --outFile ../js/main.js
+tsc item.ts book.ts movie.ts main.ts --outFile ../js/main.js
+node ../js/main.js
---
+++ src/lab7_task2/movie.ts
@@ -0,0 +1,7 @@
+class Movie implements Item {
+    constructor(public year, public title, public director) { }
+    display() {
+        console.log(`${this.year}, ${this.title}, ${this.director}`)
+    }
+
+}
