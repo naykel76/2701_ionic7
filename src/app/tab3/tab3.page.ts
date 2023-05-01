3f64a26d4031c397f6078fdf3cb217b01a639db0
Nathan <nathan@naykel.com.au>
2023-05-01 17:31:54 +1000

    lab 8 task 1

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicModule, IonPopover } from '@ionic/angular';
+import { format, parseISO } from 'date-fns';
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss'],
-  standalone: true,
-  imports: [IonicModule],
+    selector: 'app-tab3',
+    templateUrl: 'tab3.page.html',
+    standalone: true,
+    imports: [IonicModule],
 })
 export class Tab3Page {
-  constructor() {}
+
+    @ViewChild(IonPopover) popover: IonPopover;
+    reminder;
+
+    constructor() { }
+
+    /**
+     * set reminder as human readable string
+     */
+    setReminder(value) {
+        this.reminder = format(parseISO(value), 'dd/MM/yyyy');
+    }
+
 }
+
+
