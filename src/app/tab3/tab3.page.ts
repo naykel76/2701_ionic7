647b9dab57d8bb5e7a7d81584cb87528b463605d
Nathan <nathan@naykel.com.au>
2023-05-02 07:14:57 +1000

    wip

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicModule, IonPopover } from '@ionic/angular';
 import { format, parseISO } from 'date-fns';
+import { StorageService } from '../services/storage.service';
 
 @Component({
     selector: 'app-tab3',
@@ -10,18 +11,40 @@ import { format, parseISO } from 'date-fns';
 })
 export class Tab3Page {
 
-    @ViewChild(IonPopover) popover: IonPopover;
-    reminder;
+    name: any;
 
-    constructor() { }
+    constructor(private storageService: StorageService) { }
 
-    /**
-     * set reminder as human readable string
-     */
+    async setValue(key: string, value: any) {
+        await this.storageService.set(key,  value);
+    }
+
+    async getName() {
+        this.name = await this.storageService.get('name');
+    }
+
+    async removeValue(key: string) {
+        await this.storageService.remove(key);
+    }
+
+    async clearStorage() {
+        await this.storageService.clear();
+    }
+
+    // /**
+    //  * set reminder as human readable string
+    //  */
     setReminder(value) {
-        this.reminder = format(parseISO(value), 'dd/MM/yyyy');
+        //     this.reminder = format(parseISO(value), 'dd/MM/yyyy');
     }
 
 }
 
+// @ViewChild(IonPopover) popover: IonPopover;
 
+    // remember;
+    // showNotifications;
+
+    // name: string = 'Bill';
+    // remember: string = '22/05/2023';
+    // showNotifications: boolean = true;
