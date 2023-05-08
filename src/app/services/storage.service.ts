2fb674f2b33ff32c7e9a5ad3f183358e7ac064f7
Nathan <nathan@naykel.com.au>
2023-05-08 11:22:07 +1000

    housekeeping

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -12,23 +12,23 @@ export class StorageService {
         await this.storage.create();
     }
 
-    public async set(key: string, value: any) {
+    async set(key: string, value: any) {
         let result = await this.storage?.set(key, value);
     }
 
-    public async get(key: string) {
+    async get(key: string) {
         return await this.storage?.get(key);
     }
 
-    public async remove(key: string) {
+    async remove(key: string) {
         let value = await this.storage?.remove(key);
     }
 
-    public async clear() {
+    async clear() {
         let value = await this.storage?.clear();
     }
 
-    public async keys() {
+    async keys() {
         return await this.storage?.keys();
     }
 }
