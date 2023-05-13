2e6ae7e596c87979ebba1380c7bf9a49e5909a4c
Nathan <nathan@naykel.com.au>
2023-05-13 12:40:54 +1000

    lab 9 task 4

--- src/app/chart/chart.page.ts
+++ src/app/chart/chart.page.ts
@@ -2,23 +2,36 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import Chart from 'chart.js/auto';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-chart',
     template: `
     <ion-content [fullscreen]="true">
-            <ion-button (click)="manualTest()">Add Chart</ion-button>
+            <ion-item>
+                <ion-input [(ngModel)]="heightData.age" label="Age" type="tel"></ion-input>
+            </ion-item>
+            <ion-item>
+                <ion-input [(ngModel)]="heightData.height" label="Height" type="tel"></ion-input>
+            </ion-item>
+
+            <ion-button (click)="add()">Add Chart</ion-button>
             <ion-card>
                 <canvas #myChart height=300></canvas>
             </ion-card>
     </ion-content>`,
     standalone: true,
-    imports: [IonicModule, CommonModule]
+    imports: [IonicModule, CommonModule, FormsModule]
 })
 export class ChartPage implements OnInit {
 
     @ViewChild('myChart', { static: true }) canvas: any;
     chart: any;
+
+    heightData: any = {
+        age: '',
+        height: '',
+    };
 
     data = [
         { age: 14, height: 110 },
@@ -43,20 +56,23 @@ export class ChartPage implements OnInit {
         });
     }
 
-    // if you add the chart manually there are no errors
-    manualTest() {
-        //     this.chart = new Chart(this.canvas.nativeElement, {
-        //         type: 'bar',
-        //         data: {
-        //             labels: this.data.map(row => row.year),
-        //             datasets: [
-        //                 {
-        //                     label: 'Acquisitions by year',
-        //                     data: this.data.map(row => row.count)
-        //                 }
-        //             ]
-        //         }
-        //     });
+    updateChart() {
+        // update the labels
+        this.chart.data.labels = this.data.map(row => row.age);
+        // update the display data
+        this.chart.data.datasets[0].data = this.data.map(row => row.height);
+        // run the chart update method
+        this.chart.update();
+    }
+
+    add() {
+        this.data.push({ age: this.heightData.age, height: this.heightData.height });
+        this.updateChart();
+        // reset fields
+        this.heightData = {
+            age: '',
+            height: '',
+        };
     }
 
 }
